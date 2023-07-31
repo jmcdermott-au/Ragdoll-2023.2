d54e8c30430ffd363d931f84362db89c3e120d28
jmcdermott-au <63567692+jmcdermott-au@users.noreply.github.com>
2023-07-31 16:58:52 +1000

    ehuihoied

--- Ragdoll2023.2/Assets/Scripts/PhysicsScorer.cs
+++ Ragdoll2023.2/Assets/Scripts/PhysicsScorer.cs
@@ -5,12 +5,55 @@ using UnityEngine;
 
 public class PhysicsScorer : MonoBehaviour
 {
-    public Joint singleJoint;
+
+    public struct JointData
+    {
+        Joint singleJoints;
+        float previousForce;
+    }
+
+    JointData[] jointData;
+
+    public Transform ragdoll;
+    
 
     public TMP_Text forceScore;
 
-    private void Update()
+    
+
+    public float score;
+
+    private void Start()
     {
-        forceScore.text = singleJoint.currentForce.magnitude.ToString();
+        Joint[] joints = ragdoll.GetComponentsInChildren<Joint>();
+        jointData = new JointData[joints.Length];
+
+        for (int index = 0 ; index < joints.Length; index++ )
+        {
+            Joint joint = joints[index];
+            jointData[index] = new JointData();
+            jointData[index].singlejoints = joint;
+            jointData[index].previousForce = 0f;
+        }
+
+        
     }
+
+    private void FixedUpdate()
+    {
+
+        float tempscore = Mathf.Abs(previousForce - singleJoint.currentForce.magnitude);
+
+
+        if (tempscore >= 100)
+
+        {
+            score += tempscore;
+        }
+        previousForce = singleJoint.currentForce.magnitude;
+
+        forceScore.text = score.ToString();
+    }
+
+
 }
